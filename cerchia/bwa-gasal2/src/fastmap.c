e42d0f079261d1d2cffed10d694e5562bad4ce45
OrangeLuis <luigimaiese@hotmail.it>
2021-10-25 15:07:44 +0200

    modifica fastmap.c pt2

--- cerchia/bwa-gasal2/src/fastmap.c
+++ cerchia/bwa-gasal2/src/fastmap.c
@@ -16,7 +16,7 @@
 
 #include "../GASAL2/src/gasal_align.h"
 
-#include "../GASAL2/src/libStatGen/include/SamFile.h
+#include "../GASAL2/src/libStatGen/include/SamFile.h"
 #include "../GASAL2/src/libStatGen/include/SamFileHeader.h"
 
 // J.L. 2019-01-13 - select GPU when using more than one. Use the first available (0) by default.
