f41019e0ab1d37eab5feac804c0de9c51c341512
OrangeLuis <luigimaiese@hotmail.it>
2021-10-28 18:12:02 +0200

    modifica string in std:string di fastmap.c

--- copy_bwa/bwa-gasal2/src/fastmap.c
+++ copy_bwa/bwa-gasal2/src/fastmap.c
@@ -14,10 +14,10 @@
 #include "kseq.h"
 //#include <omp.h>
 
-#include "../GASAL2/src/gasal_align.h"
+#include "gasal_align.h"
 
-#include "../GASAL2/src/libStatGen/include/SamFile.h"
-#include "../GASAL2/src/libStatGen/include/SamFileHeader.h"
+#include "libStatGen/include/SamFile.h"
+#include "libStatGen/include/SamFileHeader.h"
 
 // J.L. 2019-01-13 - select GPU when using more than one. Use the first available (0) by default.
 #define GPU_SELECT (0)
@@ -515,7 +515,7 @@ void protein_aln(int argc, char *argv[]) {
 	std::string myInLine;
 	int largestA = 0, largestB = 0, totSizeA = 0, totSizeB = 0;
 
-	ifstream ref_file(refFile), quer_file(queFile);
+	std::ifstream ref_file(refFile), quer_file(queFile);
 
 	if(ref_file.is_open()) {
 		while(getline(ref_file, myInLine)) {
@@ -536,7 +536,7 @@ void protein_aln(int argc, char *argv[]) {
 	}
 
 	if(quer_file.is_open()) {
-		while(getline(quer_file, myInLine)) {
+		while(getline(que_file, myInLine)) {
 			if(myInLine[0] == '>') {
 				continue;
 			} else {
